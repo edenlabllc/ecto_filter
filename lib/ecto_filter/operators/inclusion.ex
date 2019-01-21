783a0a0e2a27ba4eeaae8444c121b17d0c61c19f
Myroslav Kiurchev <kiurchv@gmail.com>
2019-01-21 15:10:53 +0200

    chore: remove commented clauses from inclusion operator

--- lib/ecto_filter/operators/inclusion.ex
+++ lib/ecto_filter/operators/inclusion.ex
@@ -10,22 +10,6 @@ defmodule EctoFilter.Operators.Inclusion do
       def apply(query, {field, :contains, value}, {:array, _}, _) do
         where(query, [..., r], ^value in field(r, ^field))
       end
-
-      # def apply(query, {:date, field, :in, %Date.Interval{first: %Date{} = first, last: %Date{} = last}}) do
-      #   where(query, [..., r], fragment("? <@ daterange(?, ?, '[]')", field(r, ^field), ^first, ^last))
-      # end
-
-      # def apply(query, {:date, field, :in, %Date.Interval{first: %Date{} = first}}) do
-      #   where(query, [..., r], fragment("? <@ daterange(?, 'infinity', '[)')", field(r, ^field), ^first))
-      # end
-
-      # def apply(query, {:date, field, :in, %Date.Interval{last: %Date{} = last}}) do
-      #   where(query, [..., r], fragment("? <@ daterange('infinity', ?, '(]')", field(r, ^field), ^last))
-      # end
-
-      # def apply(query, {{:array, _}, field, :contains, value}) do
-      #   where(query, [..., r], fragment("? @> ?", field(r, ^field), ^value))
-      # end
     end
   end
 end
