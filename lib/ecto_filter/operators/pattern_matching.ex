af715abee2bc928879dbb672ce3427daa13d45f6
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-02-28 18:05:08 +0200

    chore: extract sanitize function

--- lib/ecto_filter/operators/pattern_matching.ex
+++ lib/ecto_filter/operators/pattern_matching.ex
@@ -4,10 +4,12 @@ defmodule EctoFilter.Operators.PatternMatching do
   defmacro __using__(_) do
     quote do
       def apply(query, {field, :like, value}, _, _) do
-        value = String.replace(value, "%", "\\%")
+        value = sanitize_like_input(value)
         
         where(query, [..., r], ilike(field(r, ^field), ^"%#{value}%"))
       end
+
+      defp sanitize_like_input(string), do: String.replace(string, "%", "\\%")
     end
   end
 end
