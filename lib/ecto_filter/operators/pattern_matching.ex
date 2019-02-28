6435424a2e1761ad97c514b56931fd5b27ba0352
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-02-28 17:58:55 +0200

    feat: sanitize like query

--- lib/ecto_filter/operators/pattern_matching.ex
+++ lib/ecto_filter/operators/pattern_matching.ex
@@ -4,6 +4,8 @@ defmodule EctoFilter.Operators.PatternMatching do
   defmacro __using__(_) do
     quote do
       def apply(query, {field, :like, value}, _, _) do
+        value = String.replace(value, "%", "\\%")
+        
         where(query, [..., r], ilike(field(r, ^field), ^"%#{value}%"))
       end
     end
